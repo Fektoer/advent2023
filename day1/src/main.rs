d5bedd319a43bf1194a316c968aa19dbcd94f6a8
Wouter Luijten <wouterluijten@hotmail.com>
2023-12-01 16:03:37 +0100

    1

---
+++ day1/src/main.rs
@@ -0,0 +1,46 @@
+pub fn main() {
+    
+    let a = include_str!("../input.txt")
+        .lines()
+        .map(|calibration| extract_digits(calibration, false))
+        .sum::<i32>();
+
+    let b = include_str!("../input.txt")
+    .lines()
+    .map(|calibration| extract_digits(calibration, true))
+    .sum::<i32>();
+    // Day1a
+    println!("{:?}", a);
+    println!("{:?}", b);
+}
+
+fn extract_digits(line: &str, parse: bool) -> i32 {
+    
+    let line_to_parse = if parse { parse_words(line) } else { line.to_string() };
+    let digits = line_to_parse
+        .chars()
+        .filter(|c| c.is_digit(10))
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<u32>>();
+
+    if let (Some(first_digit), Some(last_digit)) = (digits.first(), digits.last()) {
+        return (first_digit.to_string() + last_digit.to_string().as_str())
+            .parse::<i32>()
+            .unwrap();
+    } else {
+        return 0;
+    }
+}
+
+fn parse_words(line: &str) -> String {
+    return line.to_lowercase()
+    .replace("one", "o1e")
+    .replace("two", "t2o")
+    .replace("three", "t3e")
+    .replace("four", "f4r")
+    .replace("five", "f5e")
+    .replace("six", "s6x")
+    .replace("seven", "s7n")
+    .replace("eight", "e8t")
+    .replace("nine", "n9e");
+}
