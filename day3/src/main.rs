04bc280c2d93180dcf22d6ab46225b4b2708a988
Wouter Luijten <wouterluijten@hotmail.com>
2023-12-05 14:16:20 +0100

    3b

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,15 +1,18 @@
 use regex::Regex;
+use std::collections::HashMap;
 
 fn main() {
     //let parts = include_str!("../testInput.txt").lines();
     let parts = include_str!("../input.txt").lines();
-    let parts3a = parts.clone();
+    let cloned_parts = parts.clone();
 
     let matrix = parts
         .map(|part| part.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
 
-    let sum_parts = parts3a
+    let mut gears: HashMap<String, Vec<&str>> = HashMap::new();
+
+    let sum_parts = cloned_parts
         .enumerate()
         .map(|(row_index, part)| {
             let re = Regex::new(r"(\d+)").unwrap();
@@ -18,7 +21,7 @@ fn main() {
                 let cap_index = cap.get(1).unwrap().start();
                 let cap_value = cap.get(1).unwrap().as_str();
 
-                let mut surrounding_chars: Vec<char> = Vec::new();
+                let mut surrounding_chars: Vec<(String, char)> = Vec::new();
 
                 // line above
                 if row_index > 0 {
@@ -48,33 +51,71 @@ fn main() {
                     ));
                 }
 
-                if surrounding_chars
+                let filtered_chars = surrounding_chars
                     .iter()
-                    .filter(|c| !c.is_digit(10) && c.to_string() != ".")
-                    .collect::<Vec<_>>()
-                    .len() > 0
-                {
+                    .filter(|(_, c)| !c.is_digit(10) && c.to_string() != ".")
+                    .collect::<Vec<_>>();
+
+                if filtered_chars.len() > 0 {
                     intermediate_sum += cap_value.parse::<u32>().unwrap();
                 }
+
+                let _ = filtered_chars
+                    .iter()
+                    .map(|(key, c)| {
+                        if c.to_string() == "*" {
+                            if let Some(values) = gears.get_mut(key) {
+                                values.push(cap_value);
+                            } else {
+                                gears.insert(key.to_string(), vec![cap_value]);
+                            }
+                        }
+                    })
+                    .collect::<Vec<_>>();
             }
             return intermediate_sum;
         })
         .sum::<u32>();
 
     println!("3A {:?}", sum_parts);
+
+    let sum_gears =
+        gears
+            .iter()
+            .filter(|(_, values)| values.len() == 2)
+            .fold(0, |mut acc, result| {
+                acc += result.1[0].parse::<u32>().unwrap() * result.1[1].parse::<u32>().unwrap();
+                acc
+            });
+
+    println!("3B {}", sum_gears);
 }
 
-fn parse_line(matrix: Vec<Vec<char>>, row_index: usize, start: usize, end: usize) -> Vec<char> {
-    let mut vec_chars: Vec<char> = Vec::new();
+fn parse_line(
+    matrix: Vec<Vec<char>>,
+    row_index: usize,
+    start: usize,
+    end: usize,
+) -> Vec<(String, char)> {
+    let mut vec_chars: Vec<(String, char)> = Vec::new();
     if start > 0 {
-        vec_chars.push(matrix[row_index][start - 1])
+        vec_chars.push((
+            row_index.to_string() + (start - 1).to_string().as_str(),
+            matrix[row_index][start - 1],
+        ))
     }
-    if end < matrix[row_index].len()-1 {
-        vec_chars.push(matrix[row_index][end + 1])
+    if end < matrix[row_index].len() - 1 {
+        vec_chars.push((
+            row_index.to_string() + (end + 1).to_string().as_str(),
+            matrix[row_index][end + 1],
+        ))
     }
 
     for index in start..=end {
-        vec_chars.push(matrix[row_index][index])
+        vec_chars.push((
+            row_index.to_string() + index.to_string().as_str(),
+            matrix[row_index][index],
+        ))
     }
     return vec_chars;
 }
